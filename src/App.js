e37ac49885d9569a79b1fcb627a8a282af233906
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-05-02 15:29:39 +0100

    Create the Meals,Meals-Summary and AvailableMeals components and styled it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from 'react';
 import Header from './coponents/Layout/Header';
+import Meals from './coponents/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
